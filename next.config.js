65121044750692a2fabd8c8cc279c77998929124
Sushmoy <80288978+isushmoy@users.noreply.github.com>
2024-10-29 00:38:15 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,38 +4,6 @@ const nextConfig = {
   swcMinify: true,
   basePath: "/Nextfolio",
   output: "export",
-  async rewrites() {
-    return [
-      {
-        source: "/rss.xml",
-        destination: "/feed/rss.xml",
-      },
-      {
-        source: "/atom.xml",
-        destination: "/feed/atom.xml",
-      },
-      {
-        source: "/feed.json",
-        destination: "/feed/feed.json",
-      },
-      {
-        source: "/rss",
-        destination: "/feed/rss.xml",
-      },
-      {
-        source: "/feed",
-        destination: "/feed/rss.xml",
-      },
-      {
-        source: "/atom",
-        destination: "/feed/atom.xml",
-      },
-      {
-        source: "/json",
-        destination: "/feed/feed.json",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
