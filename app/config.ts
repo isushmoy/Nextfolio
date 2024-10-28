395702b818054d7040e74c770fb3e5677812a1b4
Sushmoy <80288978+isushmoy@users.noreply.github.com>
2024-10-29 00:18:51 +0600

    social

--- app/config.ts
+++ app/config.ts
@@ -8,9 +8,9 @@ export const metaData = {
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/mhsushmoy",
+  github: "https://github.com/isushmoy/",
+  instagram: "https://www.instagram.com/isushmoy/",
+  linkedin: "https://www.linkedin.com/sushmoy/",
+  email: "mailto:m.hossain.sushmoy@gmail.com",
 };
